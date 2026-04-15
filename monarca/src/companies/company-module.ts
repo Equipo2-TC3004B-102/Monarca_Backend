7a5ad81b0d8ed6bac9b52891345568e226bc7f22
Julio Rodriguez <jcrdzfigueroa@gmail.com>
2026-04-15 13:41:18 -0600

    feat(companies): add companies CRUD module and endpoints

--- monarca/src/companies/company-module.ts
+++ monarca/src/companies/company-module.ts
@@ -5,15 +5,19 @@
  *              to company data.
  * Authors: Debug Studio Team
  * Last Modification made:
- * 14/04/2026 [Julio Rodríguez] Created the CompaniesModule to manage company-related data and functionality.
+ * 15/04/2026 [Julio Rodríguez] Added the CompaniesModule to register the Company entity and provide services for company operations.
  */
 
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Company } from './entity/company.entity';
+import { CompaniesController } from './companies.controller';
+import { CompaniesService } from './companies.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Company])],
-  exports: [TypeOrmModule],
+  controllers: [CompaniesController],
+  providers: [CompaniesService],
+  exports: [TypeOrmModule, CompaniesService],
 })
 export class CompaniesModule {}
